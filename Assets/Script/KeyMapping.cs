c235cc3e36e0b4fb6dd1b0f7ba074c79263c5828
XcrossD <dagger1357@hotmail.com>
2014-04-12 23:05:53 +0800

    Correct moving & turning (no trigger no kinect)

--- Assets/Script/KeyMapping.cs
+++ Assets/Script/KeyMapping.cs
@@ -7,7 +7,7 @@ public class KeyMapping : MonoBehaviour {
 	public PlatformManager pM;
 	public Vector3 newPositionLeft, newPositionRight, oldPosition, newPosition;
 	public float turnPosition, checkPosition;
-	public bool isRight, isLeft, inputRight;
+	public bool isRight, isLeft, rotationInputRight, positionInputRight;
 	public Quaternion rightRotation, leftRotation;
 
 	// Use this for initialization
@@ -22,8 +22,23 @@ public class KeyMapping : MonoBehaviour {
 		//PositionChanging();
 		//RotationChanging();
 		if(Input.GetKeyDown(KeyCode.Q)){
-			inputRight = false;
-			RotationChanging(inputRight);
+			rotationInputRight = false;
+			RotationChanging(rotationInputRight);
+		}
+
+		if(Input.GetKeyDown(KeyCode.E)){
+			rotationInputRight = true;
+			RotationChanging(rotationInputRight);
+		}
+
+		if(Input.GetKeyDown(KeyCode.A)){
+			positionInputRight = false;
+			PositionChanging(positionInputRight);
+		}
+
+		if(Input.GetKeyDown(KeyCode.D)){
+			positionInputRight = true;
+			PositionChanging(positionInputRight);
 		}
 
 	}
@@ -31,7 +46,8 @@ public class KeyMapping : MonoBehaviour {
 	public void PositionChanging(bool inputRight){
 
 		oldPosition = runner.transform.localPosition;
-		if(Vector3.Dot(runner.transform.forward, Vector3.forward) == 0){
+		Debug.Log (Vector3.Dot(runner.transform.forward, Vector3.forward));
+		if(runner.rigidbody.velocity[0] == 10 || runner.rigidbody.velocity[0] == -10){
 			if(isRight){
 				newPositionRight = oldPosition;
 				oldPosition.z -= 2.5f;
@@ -63,7 +79,7 @@ public class KeyMapping : MonoBehaviour {
 			}
 		}
 		
-		if(!inputRight || Input.GetKeyDown(KeyCode.A)){
+		if(!inputRight){
 			runner.rigidbody.MovePosition(newPositionLeft);
 			if(isRight){
 				isRight = false;
@@ -72,7 +88,7 @@ public class KeyMapping : MonoBehaviour {
 			}
 
 		}
-		if(inputRight || Input.GetKeyDown(KeyCode.D)){
+		if(inputRight){
 			runner.rigidbody.MovePosition(newPositionRight);
 			if(isLeft){
 				isLeft = false;
@@ -86,19 +102,28 @@ public class KeyMapping : MonoBehaviour {
 	public void RotationChanging(bool inputRight){
 		BasicMovementForEthan ethan = runner.GetComponent<BasicMovementForEthan>();
 		if(!inputRight){
-		
-			Debug.Log("turn left");
 			Vector3 newSpeed = Vector3.Cross (ethan.speed,new Vector3(0,1,0));
-			runner.rigidbody.MoveRotation(Quaternion.FromToRotation(ethan.speed,newSpeed));
-			Debug.Log(ethan.speed);
-			Debug.Log (newSpeed);
-			Debug.Log (Quaternion.FromToRotation(ethan.speed,newSpeed));
+			runner.transform.Rotate(Vector3.up, -90);
 			ethan.speed = newSpeed;
 			
 		}else{
 			Vector3 newSpeed = Vector3.Cross (new Vector3(0,1,0),ethan.speed);
-			runner.rigidbody.MoveRotation(Quaternion.FromToRotation(ethan.speed,newSpeed));
+			runner.transform.Rotate(Vector3.up, 90);
 			ethan.speed = newSpeed;
+		}
+
+		if(runner.rigidbody.velocity[0] == 10 || runner.rigidbody.velocity[0] == -10){
+			Vector3 temp = runner.transform.localPosition;
+			temp.z = pM.nextPosition.z;
+			runner.transform.localPosition = temp;
+			isRight = false;
+			isLeft = false;
+		}else{
+			Vector3 temp = runner.transform.localPosition;
+			temp.x = pM.nextPosition.x;
+			runner.transform.localPosition = temp;
+			isRight = false;
+			isLeft = false;
 		}
 	}
 }
