de7d3c2bbab2cc86176328dfd65b1e3e24d6ab7a
RexWong1991 <rexwong1991@gmail.com>
2014-04-13 22:01:01 +0800

    deleted the useless

---
+++ Assets/platformTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformTrigger : MonoBehaviour {
+
+	private HashIDs hash; 
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTiggerEnter(Collider other){
+		//if (other.tag == "GameController")
+
+
+
+	}
+
+}
