979f28f383b5b9cdf45b7ce87d8a044c68a49bf1
XcrossD <dagger1357@hotmail.com>
2014-04-18 23:01:27 +0800

    Basic obstacle manager added

--- Assets/Script/ObstaclesManager.cs
+++ Assets/Script/ObstaclesManager.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class ObstaclesManager : MonoBehaviour {
 
+	public PlatformManager pM;
+	public GameObject runner;
+	public Transform trunk;
+	public float time, timeTime;
+	public Vector3 location;
+
 	// Use this for initialization
 	void Start () {
-	
+		time = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		timeTime = Time.time;
+		if(Time.time > time){
+			appear ();
+			time += Random.Range(3,8);
+		}
+
+	}
+
+	private void appear(){
+		location = runner.transform.localPosition;
+		location.y += 5.0f;
+
+		location += pM.currentDirection * 25f;
+		Transform o = (Transform)Instantiate(trunk);
+		o.transform.localPosition = location;
 	}
 }
