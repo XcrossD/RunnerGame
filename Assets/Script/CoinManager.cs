b2486569b9f9055bde4bb618a10ed068552427c3
XcrossD <dagger1357@hotmail.com>
2014-04-22 21:08:19 +0800

    Destroy coins after ethan passed them

--- Assets/Script/CoinManager.cs
+++ Assets/Script/CoinManager.cs
@@ -65,5 +65,4 @@ public class CoinManager : MonoBehaviour {
 
 	}
 
-
 }
--- Assets/Script/basic script/GetCoin.cs
+++ Assets/Script/basic script/GetCoin.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class GetCoin : MonoBehaviour {
 
+	public GameObject runner, pM;
+	public PlatformManager pMScript;
+
 	private CoinCount moneyCount;
+	private float runnerObjectDistance;
 	
 	void Awake () {
 	}
 	
 	void Start (){
+		runner = GameObject.Find("char_ethan modified");
+		pM = GameObject.Find("PlatformManager");
+		pMScript = pM.GetComponent<PlatformManager>();
+	}
+
+	void Update (){
+		Destroy();
 	}
 	
 	//destroy the the coin when controller charater touched it
@@ -19,6 +30,20 @@ public class GetCoin : MonoBehaviour {
 			
 			other.GetComponent<CoinCount>().money ++;	
 			GameObject.Destroy(gameObject, 0f);
+		}
+	}
+
+	public void Destroy (){
+		if(pMScript.currentDirection[0] == 0){
+			runnerObjectDistance = (runner.transform.localPosition.z - transform.localPosition.z) * pMScript.currentDirection[2];
+			if(runnerObjectDistance > 10f){
+				GameObject.Destroy(gameObject, 0f);
+			}
+		}else{
+			runnerObjectDistance = (runner.transform.localPosition.x - transform.localPosition.x) * pMScript.currentDirection[0];
+			if(runnerObjectDistance > 10f){
+				GameObject.Destroy(gameObject, 0f);
+			}
 		}
 	}
 
